544c4339a2f2863a1f642f55362602d7c48a4ac6
christy michel <christy.michel@sfr.fr>
2019-12-03 14:22:37 +0100

    validatesok

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
-  belongs_to :parent
+  belongs_to :user
   belongs_to :nanny
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :start_date, uniqueness: { scope: :end_date }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  has_many :nannies, through: :reservations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
