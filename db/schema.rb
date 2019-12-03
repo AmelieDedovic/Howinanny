02e3387ee322087d72bf4c7fa2a6d327c42a0dfc
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-12-03 12:29:09 +0100

    did the setup and the 3 core models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_101350) do
+ActiveRecord::Schema.define(version: 2019_12_03_112601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "nannies", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "experience"
+    t.integer "price_per_day"
+    t.string "city"
+    t.date "birth_date"
+    t.string "moving_area"
+    t.text "description"
+    t.index ["email"], name: "index_nannies_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_nannies_on_reset_password_token", unique: true
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "state"
+    t.integer "total_price"
+    t.bigint "user_id"
+    t.bigint "nanny_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["nanny_id"], name: "index_reservations_on_nanny_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +54,12 @@ ActiveRecord::Schema.define(version: 2019_12_03_101350) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "nannies"
+  add_foreign_key "reservations", "users"
 end
---
+++ test/models/nanny_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NannyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
