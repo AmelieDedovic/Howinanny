2310a038ff7d8ac7b2185f693d4536d7805dc8ad
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-12-11 15:10:24 +0100

    avec lapi key cest mieux

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://howinanny.herokuapp.com" }
    config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = { api_key: 'api-myapikey',
+  config.action_mailer.mailgun_settings = { api_key: ENV["MAILGUN_API_KEY"],
     domain: 'mydomain.com', }
 
   # Settings specified here will take precedence over those in config/application.rb.
