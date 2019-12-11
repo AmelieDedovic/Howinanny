edeba9154674f18571609902541407a2a9012de4
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-12-11 15:58:37 +0100

    changed domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://howinanny.herokuapp.com" }
    config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = { api_key: ENV["MAILGUN_API_KEY"],
-    domain: 'mydomain.com', }
+    domain: 'www.howinanny.com', }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
